ec6c97138ddbde457565189d38ae72d2439c1099
Sudip Bera <70878788+SudipBera083@users.noreply.github.com>
2023-02-20 22:16:53 +0530

    Delete src directory

--- src/models/LoginUsers.js
+++
@@ -1,8 +0,0 @@
-const mongoose =require("mongoose");
-
-
-const Loginschema = new mongoose.Schema({ user: 'string', password: 'string' });
-
-const login = new mongoose.model('login', Loginschema);
-
-module.exports = login;
